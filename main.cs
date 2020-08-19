424e7b71e0cda82a4785fb29948bebebc27859a0
OseiasMzM <multarisnoiado@gmail.com>
2020-08-19 00:40:02 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,18 @@
+using System;
+class MainClass {
+
+  public static void ExibeNumero (float numero) {
+    Console.WriteLine (" O número informado foi {0}", numero);
+    }
+
+  //COORDENAÇÃO
+  public static void Main () {
+    float numero;
+    Console.Write("Digite um número: ");
+    //TYPECAST
+    numero = float.Parse(Console.ReadLine());
+    ExibeNumero(numero);
+    
+  }
+
+}
